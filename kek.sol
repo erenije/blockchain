89c372c63ca8b67ad7c67866cc68e2da54f94021
erenije <47377199+erenije@users.noreply.github.com>
2021-03-23 13:22:27 +0900

    Update kek.sol

--- kek.sol
+++ kek.sol
@@ -55,6 +55,7 @@ contract Test is Owned
         string nameEmployee;
         string position;
         string phoneNumber;
+        bool isset;
     }
     
     mapping(address => Employee) private employees;
@@ -62,6 +63,18 @@ contract Test is Owned
     mapping(address => Request) private requests;
     mapping(string => Home) private homes;
     mapping(string => Ownership[]) private ownerships;
+    mapping(uint => address) private reqCase;
+    
+    
+    modifier OnlyEmployee {
+        require(
+            employees[msg.sender].isset != false,
+            'Only Employee can run this function'
+            );
+        _;
+    }
+    
+    uint reqid = 0;
     
     function AddHome(string memory _adr, uint _area, uint _cost) public {
         Home memory h;
@@ -80,7 +93,9 @@ contract Test is Owned
         d.nameEmployee = _nameEmployee;
         d.position = _position;
         d.phoneNumber = _phoneNumber;
+        d.isset = true;
         employees[empl] = d;
+        
     }
     
     function EditEmployee(address empl, string memory _nameEmployee, string memory _position, string memory _phoneNumber) public OnlyOwner{
@@ -89,8 +104,45 @@ contract Test is Owned
         employees[empl].phoneNumber = _phoneNumber;
     }
     
+    function DeleteEmployee(address empl) public OnlyOwner returns(bool) {
+        if(employees[msg.sender].isset == true){
+        delete employees[empl];
+        return true;
+        }
+        return false;
+    }
+    
     function GetEmployee(address empl) public OnlyOwner returns (string memory _nameEmployee, string memory _position, string memory _phoneNumber){
         return (employees[empl].nameEmployee, employees[empl].position, employees[empl].phoneNumber);
     }
     
+    function AddHomeRequest(address empl, string memory _homeAddress, uint _area, uint _cost) public {
+        Request memory j;
+        Home memory l;
+        l.homeAddress = _homeAddress;
+        l.area = _area;
+        l.cost = _cost;
+        j.requestType = RequestType.NewHome;
+        j.home = l;
+        j.result = 1;
+        requests[empl] = j;
+        reqCase[reqid] = empl;
+        reqid++;
+        
+    }
+    
+    function GetRequests() public OnlyEmployee view returns (string[] memory reqType, string[] memory Address, uint256[] memory Area, uint256[] memory Cost){
+        string[] memory reqType = new string[](reqid);
+        string[] memory Address = new string[](reqid);
+        uint[] memory Cost = new uint[](reqid);
+        uint[] memory Area = new uint[](reqid);
+        for (uint i = 0; i < reqid; i++) 
+        {
+            reqType[i] = requests[reqCase[i]].requestType == RequestType.NewHome ? "NewHome" : "EditHome";
+            Address[i] = requests[reqCase[i]].home.homeAddress;
+            Cost[i] = requests[reqCase[i]].home.cost;
+            Area[i] = requests[reqCase[i]].home.area;
+        }
+        return (reqType, Address, Cost, Area);
+    }
 }
